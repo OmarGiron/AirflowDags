e6a6c8ffc539a8e1959a619dd83e4b4b4e721f5d
Omar Giron <omar.giron@wizeline.com>
2022-01-04 13:55:37 -0600

    updating dag to create user_purchas table

--- MyFirstPostgresDag.py
+++ MyFirstPostgresDag.py
@@ -21,8 +21,7 @@ import datetime
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 
-# create_pet_table, populate_pet_table, get_all_pets, and get_birth_date are examples of tasks created by
-# instantiating the Postgres Operator
+# create_movies_schema, create_user_purchase_table
 
 with DAG(
     dag_id="postgres_operator_dag",
@@ -30,48 +29,26 @@ with DAG(
     schedule_interval="@once",
     catchup=False,
 ) as dag:
-    # [START postgres_operator_howto_guide_create_pet_table]
-    create_pet_table = PostgresOperator(
-        task_id="create_pet_table",
-        sql="""
-            CREATE TABLE IF NOT EXISTS pet (
-            pet_id SERIAL PRIMARY KEY,
-            name VARCHAR NOT NULL,
-            pet_type VARCHAR NOT NULL,
-            birth_date DATE NOT NULL,
-            OWNER VARCHAR NOT NULL);
-          """,
+    create_movies_schema = PostgresOperator(
+        task_id="create_movies_schema",
+        sql="CREATE SCHEMA IF NOT EXISTS  movies;"
     )
-    # [END postgres_operator_howto_guide_create_pet_table]
-    # [START postgres_operator_howto_guide_populate_pet_table]
-    populate_pet_table = PostgresOperator(
-        task_id="populate_pet_table",
-        sql="""
-            INSERT INTO pet (name, pet_type, birth_date, OWNER)
-            VALUES ( 'Max', 'Dog', '2018-07-05', 'Jane');
-            INSERT INTO pet (name, pet_type, birth_date, OWNER)
-            VALUES ( 'Susie', 'Cat', '2019-05-01', 'Phil');
-            INSERT INTO pet (name, pet_type, birth_date, OWNER)
-            VALUES ( 'Lester', 'Hamster', '2020-06-23', 'Lily');
-            INSERT INTO pet (name, pet_type, birth_date, OWNER)
-            VALUES ( 'Quincy', 'Parrot', '2013-08-11', 'Anne');
-            """,
-    )
-    # [END postgres_operator_howto_guide_populate_pet_table]
-    # [START postgres_operator_howto_guide_get_all_pets]
-    get_all_pets = PostgresOperator(task_id="get_all_pets", sql="SELECT * FROM pet;")
-    # [END postgres_operator_howto_guide_get_all_pets]
-    # [START postgres_operator_howto_guide_get_birth_date]
-    get_birth_date = PostgresOperator(
-        task_id="get_birth_date",
-        sql="""
-            SELECT * FROM pet
-            WHERE birth_date
-            BETWEEN SYMMETRIC DATE '{{ params.begin_date }}' AND DATE '{{ params.end_date }}';
-            """,
-        params={'begin_date': '2020-01-01', 'end_date': '2020-12-31'},
-    )
-    # [START postgres_operator_howto_guide_get_birth_date]
 
-    create_pet_table >> populate_pet_table >> get_all_pets >> get_birth_date
-    # [END postgres_operator_howto_guide]
+    
+    create_user_purchase_table = PostgresOperator(
+        task_id="create_user_purchase_table",
+        sql= """CREATE TABLE IF NOT EXISTS movies.user_purchase 
+            (
+            invoice_number varchar(10),
+            stock_code varchar(20),
+            detail varchar(1000),
+            quantity int,
+            invoice_date timestamp,
+            unit_price numeric(8,3),
+            customer_id int,
+            country varchar(20)
+            );"""
+        
+
+    create_movies_schema >> create_user_purchase_table 
+  
