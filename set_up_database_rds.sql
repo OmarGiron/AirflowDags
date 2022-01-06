c8a50712918df9c1692622949c3db7d18bea8a39
Omar Giron <omar.giron@wizeline.com>
2022-01-06 14:42:36 -0600

    add sql file in dags folder

---
+++ set_up_database_rds.sql
@@ -0,0 +1,18 @@
+CREATE SCHEMA IF NOT EXISTS  movies;
+
+CREATE TABLE IF NOT EXISTS movies.user_purchase 
+(
+   invoice_number varchar(10),
+   stock_code varchar(20) NULL,
+   detail varchar(1000) NULL,
+   quantity int NULL,
+   invoice_date timestamp NULL,
+   unit_price numeric(8,3) NULL,
+   customer_id int NULL,
+   country varchar (20) NULL
+);
+
+TRUNCATE TABLE movies.user_purchase;
+
+CREATE EXTENSION aws_s3 CASCADE;
+
